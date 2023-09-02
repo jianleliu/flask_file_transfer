9cae8582d314dc18110ed3bf0350496756825e9e
jianleliu <143378772+jianleliu@users.noreply.github.com>
2023-09-01 19:20:51 -1000

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class AppConfig():
     """Stores parameter variables for flask app."""
     SECRET_KEY = 'my web'
     SESSION_TYPE = 'filesystem'
-    #1000 mb max
+    #1000 mb max, change the value below for higher limit
     MAX_CONTENT_LENGTH = 1024 * 1024 * 1000
     HOST = '0.0.0.0'
     PORT = 5000
@@ -13,12 +13,12 @@ class AppConfig():
 
 class DatabaseConfig():
     """Config parameters for sql connections are stored here."""
-    USER="root"
-    DATABASE="flask_project"
-    PASSWORD="asdfghjkl;'"
-    HOST="localhost"
+    USER=""
+    DATABASE=""
+    PASSWORD=""
+    HOST=""
     MAX_ALLOWED_PACKET = str(AppConfig.MAX_CONTENT_LENGTH)
 
 class SuperUser():
     """Config parameters for Admin account, used in subprocess."""
-    PASSWORD = "asdfghjkl;'"
+    PASSWORD = ""
